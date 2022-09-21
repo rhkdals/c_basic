676a00d72e5ef00b0b0f719b34d7c123459de355
rhkdals <112834719+rhkdals@users.noreply.github.com>
2022-09-21 10:48:25 +0900

    finished chapter02

---
+++ chapter 2/chapter 02/c_printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// 제어 문자 
+//  + \n : 한 줄 개행
+//  + \t : 탭(8칸 스페이스)
+
+int main(void])
+{
+    /* code */
+    printf("HELLO\n");
+    printf("gwang min \t park");
+
+    return 0;
+}
